e6667e028157acd432f30fc1f9f2a09138fadd81
Thomas Lacroix <thomas.lacroix@epitech.eu>
2026-07-07 18:37:25 -0400

    feat: fix unit tests - feature gates
    Signed-off-by: Thomas Lacroix <thomas.lacroix@epitech.eu>

--- pkg/gameserverallocations/controller_test.go
+++ pkg/gameserverallocations/controller_test.go
@@ -62,6 +62,7 @@ func TestControllerAllocator(t *testing.T) {
 
 	runtime.FeatureTestMutex.Lock()
 	defer runtime.FeatureTestMutex.Unlock()
+	require.NoError(t, runtime.ParseFeatures(string(runtime.FeatureProcessorAllocator)+"=false"))
 
 	t.Run("successful allocation", func(t *testing.T) {
 		f, gsList := defaultFixtures(4)
@@ -387,6 +388,11 @@ func TestMultiClusterAllocationFromLocal(t *testing.T) {
 func TestMultiClusterAllocationFromRemote(t *testing.T) {
 	const clusterName = "remotecluster"
 	t.Parallel()
+
+	runtime.FeatureTestMutex.Lock()
+	defer runtime.FeatureTestMutex.Unlock()
+	require.NoError(t, runtime.ParseFeatures(string(runtime.FeatureProcessorAllocator)+"=false"))
+
 	t.Run("Handle allocation request remotely", func(t *testing.T) {
 		c, m := newFakeController()
 		fleetName := addReactorForGameServer(&m)
